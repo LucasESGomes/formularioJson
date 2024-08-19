b35a24b72c1c06aa5bbf156216c66c40286e55bd
LucasESGomes <158865689+LucasESGomes@users.noreply.github.com>
2024-08-18 22:45:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function enviarFormulario() {
+
+    //Obter valores das variaveis
+    var infClientes = new Object();
+
+    infClientes.nome = document.getElementById("nome").value;
+    infClientes.sobrenome = document.getElementById("sobrenome").value;
+    infClientes.cpf = document.getElementById("cpf").value;
+    infClientes.telefone = document.getElementById("telefone").value;
+    infClientes.idade = document.getElementById("idade").value;
+    infClientes.email = document.getElementById("email").value;
+    infClientes.senha = document.getElementById("senha").value;
+    infClientes.sexo = document.getElementById("sexo").value;
+
+    //Converter String para JSON
+    var json = JSON.stringify(infClientes);
+
+    //Imprimir no console
+    console.log(json)
+    alert("Cadastro realizado com sucesso!")
+}
